0e9def28ebb379ad60f9b7b2ac4a409a631cba82
Jayant Sane <ghjsane@gmail.com>
2018-01-12 11:41:31 -0800

    (PUP-8141-followup) Based fips specific behaviors without using facts

---
+++ lib/puppet/util/fips.rb
@@ -0,0 +1,30 @@
+
+# Method to determine whether where are running on a FIPs enabled host
+
+module Puppet
+  module Util
+    module FIPS
+      def fips_enabled?
+        # We currently only plan to support FIPS on RHEL7
+        # so we try to eliminate anything un-supported using
+        # what we have available in lieu of not being able to specifically
+        # check for it. 
+        if Puppet::Util::Platform.windows?
+          false
+        end
+      
+        # REMIND - How to check if we are on posix+linux system to avoid
+        # getting surprised on any odd-ball system that might not entertain
+        # the below file query.
+      
+        fips_status_file = '/proc/sys/crypto/fips_enabled'
+      
+        if File.exist?(fips_status_file) && File.open(status_file, &:readline)[0].chr == '1'
+          true
+        else
+          false
+        end
+      end
+    end
+  end
+end
